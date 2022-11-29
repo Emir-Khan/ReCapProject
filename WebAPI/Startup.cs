a7fa30dee3f31d0fbc1584ce673c69ed02c54e5e
Emir-Khan <emirkhan5534@gmail.com>
2022-11-29 17:44:49 +0300

    Swagger Integration

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -56,7 +56,7 @@ namespace WebAPI
             services.AddDependencyResolvers(new ICoreModule[] {
                new CoreModule()
             });
-
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -65,6 +65,8 @@ namespace WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.ConfigureCustomExceptionMiddleware();
